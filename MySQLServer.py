d87a772d48405ea570586df1e729f133bf78f642
Benjamin Oscar Aidoo <benaidoojnr@gmail.com>
2025-12-11 16:47:18 +0300

    updated code again

--- MySQLServer.py
+++ MySQLServer.py
@@ -3,6 +3,13 @@ import mysql.connector
 
 create_db_query = ["CREATE DATABASE IF NOT EXISTS alx_book_store"]
 
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="yourusername",
+    password="yourpassword",
+    database="yourdatabase"
+)
+
 
         create_tble_query = "CREATE TABLE IF NOT EXISTS books (id INT AUTO_INCREMENT PRIMARY KEY," \
         "title VARCHAR(255)," \
