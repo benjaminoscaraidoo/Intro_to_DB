30d1099a512506a97816d1f1d233411be476bcd4
Benjamin Oscar Aidoo <benaidoojnr@gmail.com>
2025-12-11 17:09:01 +0300

    update code for store book

--- task_2.sql
+++ task_2.sql
@@ -1,1 +1,7 @@
-kjhas
+CREATE TABLE Authors (author_id INT AUTO_INCREMENT PRIMARY KEY, author_name VARCHAR(215))
+
+CREATE TABLE Books (book_id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(130),FOREIGN KEY (author_id) REFERENCES Authors(author_id),price DOUBLE, publication_date DATE)
+
+
+
+["CREATE TABLE ", "Authors", "author_id ", "author_name"]
