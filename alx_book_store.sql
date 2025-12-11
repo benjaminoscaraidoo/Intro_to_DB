5be88272b3814bf77d0c8bc9d3b38566bf13e10c
Benjamin Oscar Aidoo <benaidoojnr@gmail.com>
2025-12-11 16:19:34 +0300

    Updated Books store codes

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,2 +1,2 @@
-CREATE TABLE IF NOT EXISTS alx_book_store
- (book_id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(130),FOREIGN KEY(author_id) REFERENCES Authors(id),price DOUBLE,publication_date DATE)
+[CREATE TABLE IF NOT EXISTS alx_book_store]
+ (book_id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(130),FOREIGN KEY(author_id) REFERENCES Authors(id),price DOUBLE,publication_date DATE)]
