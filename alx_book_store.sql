4288c09808badcdaa6efe6879e63932921168f79
Benjamin Oscar Aidoo <benaidoojnr@gmail.com>
2025-12-11 16:21:05 +0300

    updated code for book lib

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,2 +1,6 @@
-[CREATE TABLE IF NOT EXISTS alx_book_store]
+["CREATE TABLE IF NOT EXISTS alx_book_store"]
+
+
+alx_book_store.sql doesn't contain: ["CREATE DATABASE IF NOT EXISTS alx_book_store"]
+
  (book_id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(130),FOREIGN KEY(author_id) REFERENCES Authors(id),price DOUBLE,publication_date DATE)]
