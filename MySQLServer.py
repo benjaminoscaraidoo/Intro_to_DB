70a9e96b6a611eb43a428209c8010631f80c0e29
Benjamin Oscar Aidoo <benaidoojnr@gmail.com>
2025-12-11 16:55:49 +0300

    updated code for sql create

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,7 +1,5 @@
 import mysql.connector
 
-
-create_db_query = ["CREATE DATABASE IF NOT EXISTS alx_book_store"]
 try:
     mydb = mysql.connector.connect(
         host="localhost",
@@ -9,13 +7,14 @@ try:
         password="yourpassword",
         database="yourdatabase"
     )
+
+    tbcursor = mydb.cursor()
+
+    create_db_query = "CREATE DATABASE IF NOT EXISTS alx_book_store"
+
+    tbcursor.execute(create_db_query)
+
+    tbcursor.close()
+    mydb.close()
 except mysql.connector.Error as e:
         print(e)
-
-
-
-        create_tble_query = "CREATE TABLE IF NOT EXISTS books (id INT AUTO_INCREMENT PRIMARY KEY," \
-        "title VARCHAR(255)," \
-        "author VARCHAR(255)," \
-        "ISBN VARCHAR(255))"
-        tbcursor.execute(create_tble_query)
