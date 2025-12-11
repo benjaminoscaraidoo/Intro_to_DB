5d7853db603e07d882e15d992706f2c620f4ac8b
Benjamin Oscar Aidoo <benaidoojnr@gmail.com>
2025-12-11 16:28:01 +0300

    updated code for books

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,6 +1,7 @@
 ["CREATE TABLE IF NOT EXISTS alx_book_store"]
 ["author_id ", "author_name"]
 ["Books"]
+["Customers", "customer_id ", "customer_name VARCHAR(215)", "email VARCHAR(215)", "address TEXT"]
 
 
 alx_book_store.sql doesn't contain: ["CREATE DATABASE IF NOT EXISTS alx_book_store"]
