d3108df07fba3fec4904d0652369b8492ee1a328
Benjamin Oscar Aidoo <benaidoojnr@gmail.com>
2025-12-11 16:28:55 +0300

    updated code for orders

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -2,6 +2,7 @@
 ["author_id ", "author_name"]
 ["Books"]
 ["Customers", "customer_id ", "customer_name VARCHAR(215)", "email VARCHAR(215)", "address TEXT"]
+["Orders", "order_id INT", "customer_id INT", "order_date DATE", "FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)"]
 
 
 alx_book_store.sql doesn't contain: ["CREATE DATABASE IF NOT EXISTS alx_book_store"]
