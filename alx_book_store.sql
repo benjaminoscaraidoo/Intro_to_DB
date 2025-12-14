df5f8eeffcfa0603a57adf76c79ccf9930da4ca2
Benjamin Oscar Aidoo <benaidoojnr@gmail.com>
2025-12-15 00:30:27 +0300

    updated code for orders

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -2,10 +2,10 @@ CREATE TABLE IF NOT EXISTS alx_book_store
 
 ["author_id ", "author_name"]
 ["Books"]
+["book_id INT"]
 ["Customers", "customer_id ", "customer_name VARCHAR(215)", "email VARCHAR(215)", "address TEXT"]
 ["Orders", "order_id INT", "customer_id INT", "order_date DATE", "FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)"]
 ["Order_Details", "quantity DOUBLE", "FOREIGN KEY (order_id) REFERENCES Orders(order_id)", "FOREIGN KEY (book_id) REFERENCES Books(book_id)"]
-
 
 
 
