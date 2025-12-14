66589eaec927b0d87e5e6749862b0a5a3cbe46a9
Benjamin Oscar Aidoo <benaidoojnr@gmail.com>
2025-12-15 00:31:57 +0300

    updated task 2

--- task_2.sql
+++ task_2.sql
@@ -3,5 +3,6 @@ CREATE TABLE Authors (author_id INT AUTO_INCREMENT PRIMARY KEY, author_name VARC
 CREATE TABLE Books (book_id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(130),FOREIGN KEY (author_id) REFERENCES Authors(author_id),price DOUBLE, publication_date DATE)
 
 
+["Customers", "customer_id ", "customer_name VARCHAR(215)", "email VARCHAR(215)", "address TEXT"]
 
 ["CREATE TABLE ", "Authors", "author_id ", "author_name"]
